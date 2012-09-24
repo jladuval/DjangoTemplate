97bb66ea267d2ff097fa2802845ecd967227afe3
JDuval <jladuval@gmail.com>
2012-09-24 18:21:13 +1200

    made path relative

--- settings.py
+++ settings.py
@@ -106,7 +106,7 @@ ROOT_URLCONF = 'urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wsgi.application'
 
-TEMPLATE_DIRS = ('C:/Users/Jacob/My Documents/GitHub/DjangoTemplate//templates',)
+TEMPLATE_DIRS = (os.path.join(PROJECT_ROOT, 'templates'),)
 
 INSTALLED_APPS = (
     'django.contrib.auth',
